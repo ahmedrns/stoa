a7e205a88f604edd74d6a29664754cb43e6fb216
Ahmed Tariq <ahmed@rnssol.com>
2021-10-15 11:15:39 +0500

    total voted added to proposalById API

--- src/Types.ts
+++ src/Types.ts
@@ -1522,6 +1522,16 @@ export interface IProposalAPI {
      */
     not_voted_percentage: number;
 
+    /**
+     * Voted Voted Percentage
+     */
+    voted_percentage: number;
+
+    /**
+     * Voted Voted numbers
+     */
+    total_voted: number;
+
 }
 
 /**
